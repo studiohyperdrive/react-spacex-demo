3ad0a188d656324537b8fb6b3a19b805edc943e2
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-04-16 11:39:22 +0200

    feat(router): setup

---
+++ src/router/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import { EntryView, HomeView } from '../views';
+
+export const ROUTE_PATHS = {
+	entry: '/',
+	home: '/home',
+};
+
+export const ROUTES = [
+	{ path: ROUTE_PATHS.entry, component: EntryView, exact: true }, // We need to set exact to true, otherwise all routes will only match the first
+	{ path: ROUTE_PATHS.home, component: HomeView },
+];
+
+export const AppRouter: React.FC = () => {
+	return (
+		<Router>
+			<Switch>
+				{ROUTES.map((route) => (
+					<Route key={route.path} {...route} />
+				))}
+			</Switch>
+		</Router>
+	);
+};
