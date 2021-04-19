00193939fd3a676effffe48177ad45c3dcb0bd6e
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-04-19 12:13:43 +0200

    feat(Navigation): add styling

--- src/modules/shared/components/Navigation/Navigation.tsx
+++ src/modules/shared/components/Navigation/Navigation.tsx
@@ -1,21 +1,29 @@
+import classNames from 'classnames/bind';
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import { Logo } from '../Logo';
 
+import styles from './Navigation.module.scss';
 import { NavigationProps } from './Navigation.types';
+
+const cx = classNames.bind(styles);
 
 const Navigation: React.FC<NavigationProps> = ({ logoUrl = '', items = [] }) => {
 	return (
-		<nav>
-			<div className="c-navigation__logo">
+		<nav className={cx('c-navigation')}>
+			<div className={cx('c-navigation__logo')}>
 				<Link to={logoUrl}>
 					<Logo />
 				</Link>
 			</div>
-			<div className="c-navigation__items">
+			<div className={cx('c-navigation__items')}>
 				{items.map(({ label, ...linkProps }, index) => (
-					<NavLink key={`label-${index}`} {...linkProps}>
+					<NavLink
+						key={`label-${index}`}
+						className={cx('c-navigation__link')}
+						{...linkProps}
+					>
 						{label}
 					</NavLink>
 				))}
