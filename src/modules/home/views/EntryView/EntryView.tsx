c9c0ba1c4a289eac5f960a579d276083e47a8cef
Andry Charlier <andry.charlier@studiohyperdrive.be>
2021-04-19 12:28:46 +0200

    feat(EntryView): add styling

--- src/modules/home/views/EntryView/EntryView.tsx
+++ src/modules/home/views/EntryView/EntryView.tsx
@@ -1,13 +1,18 @@
+import classNames from 'classnames/bind';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { ROUTE_PATHS } from '../../../../router';
 import { Button, Logo } from '../../../shared/components';
 
+import styles from './EntryView.module.scss';
+
+const cx = classNames.bind(styles);
+
 const EntryView: React.FC = () => {
 	return (
-		<div className="v-entry">
-			<div className="v-entry__Logo">
+		<div className={cx('v-entry', 'u-container')}>
+			<div className={cx('v-entry__logo')}>
 				<Logo />
 			</div>
 			<Link to={ROUTE_PATHS.home}>
