79b0e282b2d459dbee20f9ce97eef267661c43a7
smtsj <jo.smets@studiohyperdrive.be>
2021-04-26 15:27:26 +0200

    chore(entry): add small welcome text

--- src/modules/home/views/EntryView/EntryView.tsx
+++ src/modules/home/views/EntryView/EntryView.tsx
@@ -15,6 +15,32 @@ const EntryView: React.FC = () => {
 			<div className={cx('v-entry__logo')}>
 				<Logo />
 			</div>
+			<div className={cx('v-entry__welcome')}>
+				<h2>Hi Folks!</h2>
+				<p>
+					Via dit project proberen we om enkele basis-principes te tonen die wij
+					belangrijk vinden in React applicaties.
+				</p>
+				<p>
+					We delen ook graag de{' '}
+					<a
+						href="https://media.studiohyperdrive.be/presentations/presentatie-react.pdf"
+						target="_blank"
+						rel="noreferrer"
+					>
+						bijhorende presentatie
+					</a>{' '}
+					en de{' '}
+					<a
+						href="https://github.com/studiohyperdrive/react-spacex-demo"
+						target="_blank"
+						rel="noreferrer"
+					>
+						broncode op Github
+					</a>
+					.
+				</p>
+			</div>
 			<Link to={ROUTE_PATHS.home}>
 				<Button>Enter</Button>
 			</Link>
